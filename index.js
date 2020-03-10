41cc7642b26dbfd8381775c568c9f5a6ae4fffde
jhygt46 <infolifeoperacional@gmail.com>
2020-03-10 14:37:14 -0300

    aa

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-var http = require('http');
+//var http = require('http');
+/*
 http.createServer(function (req, res) {
     res.writeHead(301, { "Location": "https://www.izusushi.cl" });
     res.end();
 }).listen(80);
+*/
+const express = require("express");
+const app = express();
+app.listen(80, () => {
+    console.log("El servidor está inicializado en el puerto 3000");
+});
+app.get('/', function (req, res) {
+    res.send('Saludos desde express');
+});
